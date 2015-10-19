e8d854bb95fb0f149dce6eee67def7381db33dea
Pooja Reddy <pooja.penta.reddy@gmail.com>
2015-10-18 20:26:27 -0700

    initial build setup

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    entry: "./client/app.js",
+    output: {
+        filename: "public/bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style!css" },
+            { test: /\.jsx?$/,
+              exclude: /(node_modules|bower_components)/,
+              loader: 'babel'
+            },
+            { test: /\.less$/,
+              loader: "style!css!less"
+            }
+        ]
+    }
+};
